3224260ef30782b08be4b3038019ec74e6eae60d
abdul.sami <abdul.sami@telecom-sudparis.eu>
2026-04-02 16:12:06 +0200

    add ssl

--- apps/frontend/src/lib/api.ts
+++ apps/frontend/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getSession } from "next-auth/react";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://62.72.18.75:3001/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://api.escalium.io/api",
 });
 
 api.interceptors.request.use(async (config) => {
