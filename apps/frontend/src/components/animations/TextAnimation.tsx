afb8f819826dce13ab6871a5e5ac96244ffc14bd
rafuj <raihanrafuj9978@gmail.com>
2026-05-05 09:04:29 +0600

    update text-color and google button

--- apps/frontend/src/components/animations/TextAnimation.tsx
+++ apps/frontend/src/components/animations/TextAnimation.tsx
@@ -1,45 +1,45 @@
 import { motion } from "framer-motion";
 
 const TextAnimation = ({
-	text,
-	staggerChildren = 0.04,
-	delay = 0,
+  text,
+  staggerChildren = 0.04,
+  delay = 0,
 }: {
-	text: string;
-	staggerChildren?: number;
-	delay?: number;
+  text: string;
+  staggerChildren?: number;
+  delay?: number;
 }) => {
-	const container = {
-		hidden: { opacity: 0 },
-		show: {
-			opacity: 1,
-			transition: {
-				staggerChildren: staggerChildren,
-				delay: delay,
-			},
-		},
-	};
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: staggerChildren,
+        delayChildren: delay,
+      },
+    },
+  };
 
-	const item = {
-		hidden: { y: 20, opacity: 0 },
-		show: { y: 0, opacity: 1 },
-	};
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    show: { y: 0, opacity: 1 },
+  };
 
-	return (
-		<motion.div
-			initial="hidden"
-			whileInView="show"
-			variants={container}
-			viewport={{ once: true }}
-			className="inline"
-		>
-			{text.split("").map((char, index) => (
-				<motion.span key={index} variants={item}>
-					{char}
-				</motion.span>
-			))}
-		</motion.div>
-	);
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="show"
+      variants={container}
+      viewport={{ once: true }}
+      className="inline"
+    >
+      {text.split("").map((char, index) => (
+        <motion.span key={index} variants={item}>
+          {char}
+        </motion.span>
+      ))}
+    </motion.div>
+  );
 };
 
 export default TextAnimation;
--- apps/frontend/src/page-components/landing/Hero.tsx
+++ apps/frontend/src/page-components/landing/Hero.tsx
@@ -41,7 +41,10 @@ const Hero = () => {
                   </button>
                 )}
                 <h1 className="text-4xl lg:text-6xl lg:leading-[1.3] tracking-wide">
-                  <TextAnimation text="You make the music, Escalium Scales it." />
+                  <TextAnimation text="You make the music, " />
+                  <span className="text-primary">
+                    <TextAnimation text="Escalium Scales it." delay={0.8} />
+                  </span>
                 </h1>
 
                 <p className="max-w-2xl mx-auto md:text-lg lg:text-xl mt-6 mb-8 text-secondary">
